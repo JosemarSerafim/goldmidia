0e4a145e9189282729eaab853d11dc27a46c5a03
josemar serafim <josemarserafim2016@gmail.com>
2021-10-31 22:35:52 +0100

    :package: sessao serviços

---
+++ javascript.js
@@ -0,0 +1,14 @@
+function Write(element) {
+    const textArray = element.innerHTML.split('');
+    element.innerHTML = '';
+    textArray.forEach((letter, i)=>  {
+
+        setTimeout(()=>element.innerHTML += letter, 75*i);
+    });
+
+    
+}
+
+const titulo = document.querySelector('h2');
+
+Write(titulo);
