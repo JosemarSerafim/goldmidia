bb8892ae62cc96a0066bdb5d5e9f456e0fa5aa5e
josemar serafim <josemarserafim2016@gmail.com>
2021-11-15 13:02:01 +0100

    teste

--- javascript.js
+++ javascript.js
@@ -1,16 +1,42 @@
-function Write(element) {
-    const textArray = element.innerHTML.split('');
-    element.innerHTML = '';
-    textArray.forEach((letter, i)=>  {
+const typing = document.querySelector('[data-js="typing"]')
+const messages = ['A melhor escolha para a sua empresa', 'venha conhecer-nos']
+let messageIndex = 0
+let characterIndex = 0
+let currentMessage =''
+let currentCharacter = ''
 
-        setTimeout(()=>element.innerHTML += letter, 75*i);
-    });
+const type = ()=>{
 
-    
+    if(messageIndex === messages.length) {
+        messageIndex = 0;
+    }
 
-    
+    currentMessage = messages[messageIndex];
+    currentCharacter = currentMessage.slice(0, characterIndex++);
+    typing.textContent = currentCharacter;
+
+    if(currentCharacter.length === currentMessage.length) {
+        messageIndex++
+        characterIndex = 0
+    }
 }
 
-const titulo = document.querySelector('h2');
+setInterval(type, 75);
 
-Write(titulo);
+
+// function Write(element) {
+//     const textArray = element.innerHTML.split('');
+//     element.innerHTML = '';
+//     textArray.forEach((letter, i)=>  {
+
+//         setTimeout(()=>element.innerHTML += letter, 75*i);
+//     });
+
+    
+
+    
+// }
+
+// const titulo = document.querySelector('h2');
+
+// Write(titulo);
